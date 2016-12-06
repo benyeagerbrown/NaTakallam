cf2ab3438b3f1ac0ee9f8d3f75a9b7f7e6a55e67
Ben Brown <byb@bu.edu>
2016-12-05 19:13:52 -0500

    Added User class

--- app/src/main/java/com/globalappinitiative/natakallam/SignupActivity.java
+++ app/src/main/java/com/globalappinitiative/natakallam/SignupActivity.java
@@ -3,12 +3,14 @@ package com.globalappinitiative.natakallam;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class SignupActivity extends AppCompatActivity {
 
-    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_signup);
     }
+
+    // Postman, OKHttp, Volley
+    // https://guides.codepath.com/android/Consuming-APIs-with-Retrofit
 }
