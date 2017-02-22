0de4efb64c6a14280f7ab492eeef9f01e26954cd
Ben Brown <byb@bu.edu>
2017-02-21 19:12:32 -0500

    Added spinners to AddSession
    Added month and day arrays to strings

--- app/src/main/java/com/globalappinitiative/natakallam/AddSessionActivity.java
+++ app/src/main/java/com/globalappinitiative/natakallam/AddSessionActivity.java
@@ -1,7 +1,13 @@
 package com.globalappinitiative.natakallam;
 
+
+import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.view.View;
 
 public class AddSessionActivity extends AppCompatActivity {
 
@@ -10,8 +16,40 @@ public class AddSessionActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_session);
 
+        Spinner month_spinner = (Spinner) findViewById(R.id.month_spinner);
+
+        ArrayAdapter<CharSequence> month_adapter = ArrayAdapter.createFromResource(this,
+                R.array.month_array, android.R.layout.simple_spinner_item);
+
+        month_adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        month_spinner.setAdapter(month_adapter);
+
+
+        Spinner day_spinner = (Spinner) findViewById(R.id.day_spinner);
+
+        ArrayAdapter<CharSequence> day_adapter = ArrayAdapter.createFromResource(this,
+                R.array.day_array, android.R.layout.simple_spinner_item);
+
+        day_adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        day_spinner.setAdapter(day_adapter);
 
     }
+
+    public class SpinnerActivity extends Activity implements AdapterView.OnItemSelectedListener{
+
+        public void onItemSelected(AdapterView<?> parent, View view, int pos, long id) {
+
+        }
+
+        public void onNothingSelected(AdapterView<?> parent) {
+
+        }
+
+    }
+
+
 
 
 }
