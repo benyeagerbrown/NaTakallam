5ce2a2bd6667cb5f312b4b81f8de8a7a087b1214
Ben Brown <byb@bu.edu>
2017-03-13 22:02:12 -0400

    Added time picker and date picker dialogs to the AddSession activity
    Will work more on their styles and the AddSession page itself more (right now both are an ugly pink color)

--- app/src/main/java/com/globalappinitiative/natakallam/AddSessionActivity.java
+++ app/src/main/java/com/globalappinitiative/natakallam/AddSessionActivity.java
@@ -2,54 +2,35 @@ package com.globalappinitiative.natakallam;
 
 
 import android.app.Activity;
+import android.app.DatePickerDialog;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.Spinner;
+import android.text.format.DateFormat;
 import android.view.View;
+import android.app.DatePickerDialog;
+import android.app.DialogFragment;
 
-public class AddSessionActivity extends AppCompatActivity {
+public class AddSessionActivity extends AppCompatActivity  {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_session);
 
-        Spinner month_spinner = (Spinner) findViewById(R.id.month_spinner);
-
-        ArrayAdapter<CharSequence> month_adapter = ArrayAdapter.createFromResource(this,
-                R.array.month_array, android.R.layout.simple_spinner_item);
-
-        month_adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-
-        month_spinner.setAdapter(month_adapter);
-
-
-        Spinner day_spinner = (Spinner) findViewById(R.id.day_spinner);
-
-        ArrayAdapter<CharSequence> day_adapter = ArrayAdapter.createFromResource(this,
-                R.array.day_array, android.R.layout.simple_spinner_item);
-
-        day_adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-
-        day_spinner.setAdapter(day_adapter);
-
     }
 
-    public class SpinnerActivity extends Activity implements AdapterView.OnItemSelectedListener{
-
-        public void onItemSelected(AdapterView<?> parent, View view, int pos, long id) {
-
-        }
-
-        public void onNothingSelected(AdapterView<?> parent) {
-
-        }
-
+    public void showDatePickerDialog(View v) {
+        DialogFragment newFragment = new AddDateFragment();
+        newFragment.show(getFragmentManager(), "addDate");
     }
 
-
-
+    public void showTimePickerDialog(View v) {
+        DialogFragment newFragment = new AddTimeFragment();
+        newFragment.show(getFragmentManager(), "addTime");
+    }
 
 }
+
+
+
