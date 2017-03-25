8cf6e8a36339a0e7ec55624f8d53aa7403322923
brettm2277 <brettm2277@gmail.com>
2017-03-25 11:22:49 -0400

    Add preferences fragment to Settings page.

--- app/src/main/java/com/globalappinitiative/natakallam/SettingsFragment.java
+++ app/src/main/java/com/globalappinitiative/natakallam/SettingsFragment.java
@@ -1,29 +1,14 @@
 package com.globalappinitiative.natakallam;
 
-
 import android.os.Bundle;
-import android.app.Fragment;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
+import android.preference.PreferenceFragment;
 
-
-/**
- * A simple {@link Fragment} subclass.
- */
-public class SettingsFragment extends Fragment {
-
-
-    public SettingsFragment() {
-        // Required empty public constructor
-    }
-
+public class SettingsFragment extends PreferenceFragment {
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_settings, container, false);
+    public void onCreate(final Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        addPreferencesFromResource(R.xml.preferences);
     }
 
 }
