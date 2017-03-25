001d7533ec55d870d7fe5b7303c5ea4663713a98
brettm2277 <brettm2277@gmail.com>
2017-03-25 14:17:50 -0400

    Added Change Email activity.

--- app/src/main/java/com/globalappinitiative/natakallam/SettingsFragment.java
+++ app/src/main/java/com/globalappinitiative/natakallam/SettingsFragment.java
@@ -1,14 +1,81 @@
 package com.globalappinitiative.natakallam;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.ListPreference;
+import android.preference.Preference;
 import android.preference.PreferenceFragment;
+import android.preference.PreferenceGroup;
+import android.preference.PreferenceScreen;
+import android.util.Log;
 
-public class SettingsFragment extends PreferenceFragment {
+public class SettingsFragment extends PreferenceFragment implements SharedPreferences.OnSharedPreferenceChangeListener {
 
     @Override
     public void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         addPreferencesFromResource(R.xml.preferences);
+        getPreferenceScreen().getSharedPreferences().registerOnSharedPreferenceChangeListener(this);
     }
 
+    @Override
+    public void onResume() {
+        super.onResume();
+        for (int i = 0; i < getPreferenceScreen().getPreferenceCount(); i++) {
+            Preference preference = getPreferenceScreen().getPreference(i);
+            if (preference instanceof PreferenceGroup) {
+                PreferenceGroup preferenceGroup = (PreferenceGroup) preference;
+                for (int j = 0; j < preferenceGroup.getPreferenceCount(); j++) {
+                    Preference singlePreference = preferenceGroup.getPreference(j);
+                    updatePreference(singlePreference, singlePreference.getKey());
+                }
+            } else {
+                updatePreference(preference, preference.getKey());
+            }
+        }
+    }
+
+    @Override
+    public boolean onPreferenceTreeClick(PreferenceScreen preferenceScreen, Preference preference) {
+
+        String key = preference.getKey();
+        switch (key) {
+            case PreferenceKeys.email:
+                Log.d("preference", "email");
+                startActivity(new Intent(getContext(), ChangeEmailActivity.class));
+                break;
+            case PreferenceKeys.password:
+                break;
+            case PreferenceKeys.skype_id:
+                break;
+            case PreferenceKeys.phone_number:
+                break;
+            case PreferenceKeys.taboo_topics:
+                break;
+            case PreferenceKeys.about_us:
+                break;
+            case PreferenceKeys.log_out:
+                break;
+        }
+        return super.onPreferenceTreeClick(preferenceScreen, preference);
+    }
+
+    @Override
+    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
+        updatePreference(findPreference(key), key);
+    }
+
+    private void updatePreference(Preference preference, String key) {
+        if (preference == null) {
+            return;
+        }
+        if (preference instanceof ListPreference) {
+            ListPreference listPreference = (ListPreference) preference;
+            listPreference.setSummary(listPreference.getEntry());
+            return;
+        }
+        SharedPreferences sharedPreferences = getPreferenceManager().getSharedPreferences();
+        preference.setSummary(sharedPreferences.getString(key, ""));
+    }
 }
